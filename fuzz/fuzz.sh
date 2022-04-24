777bc4459c694364f7f713418c0ac5e10d9938c1
Jonathan Zhao <zhaojonathan99@gmail.com>
2022-04-24 18:35:30 -0400

    Fixed rm arg and added sock arg to docker

--- fuzz/fuzz.sh
+++ fuzz/fuzz.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker run -p -d -v /var/run/docker.sock:/var/run/docker.sock --net=host --mount "type=bind,source=/fuzz/supergraph.graphql,target=/supergraph.graphql" --r, ghcr.io/apollographql/router:v0.1.0-preview.6 -s supergraph.graphql
+docker run -p -d -v /var/run/docker.sock:/var/run/docker.sock --net=host --mount "type=bind,source=/fuzz/supergraph.graphql,target=/supergraph.graphql" --rm ghcr.io/apollographql/router:v0.1.0-preview.6 -s supergraph.graphql
 exec "$@"
